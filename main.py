cf6b2cf53af49bc399d9197734a2367b029fb30c
Indu <indulahari6@gmail.com>
2024-07-09 13:56:43 +0530

    Load/Read data from Excel to data frames

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import pandas as pd
 import glob
 
 filepaths = glob.glob("Invoices/*.xlsx")
-print(filepaths)
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
