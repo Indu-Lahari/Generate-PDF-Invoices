6ffec0c079231da622d984f875ea05f587b06ef6
Indu <indulahari6@gmail.com>
2024-07-09 23:40:24 +0530

    Final PDF Invoice

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=10)
     pdf.set_text_color(40, 40, 40)
     pdf.cell(w=30, h=8, txt=header[0], border=1)
-    pdf.cell(w=70, h=8, txt=header[1], border=1)
-    pdf.cell(w=30, h=8, txt=header[2], border=1)
+    pdf.cell(w=60, h=8, txt=header[1], border=1)
+    pdf.cell(w=40, h=8, txt=header[2], border=1)
     pdf.cell(w=30, h=8, txt=header[3], border=1)
     pdf.cell(w=30, h=8, txt=header[4], border=1, ln=1)
 
@@ -41,8 +41,8 @@ for filepath in filepaths:
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(40, 40, 40)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
@@ -51,8 +51,8 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=10)
     pdf.set_text_color(40, 40, 40)
     pdf.cell(w=30, h=8, txt="", border=1)
-    pdf.cell(w=70, h=8, txt="", border=1)
-    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
     pdf.cell(w=30, h=8, txt="", border=1)
     pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
