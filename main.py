3bf7b1c7c52c9c77eb0878a17876edc7ee8b532c
Indu <indulahari6@gmail.com>
2024-07-09 22:53:42 +0530

    Add table from Excel to PDF

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ for filepath in filepaths:
 
     #Extracting filename and invoice from paths
     filename = Path(filepath).stem
-    #Extracting invoice from filename and splitting at date
-    invoice_no = filename.split("-")[0]
-    date = filename.split("-")[1]
+    #Extracting invoice and date from filename
+    invoice_no, date = filename.split("-")
 
     #Create PDF file from Excel file
     pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -25,6 +24,15 @@ for filepath in filepaths:
 
     pdf.set_font(family="Times", style="B", size=10)
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", style="B", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     #Create PDF that creates dynamic name automatically
     pdf.output(f"PDFs/{filename}.pdf")
