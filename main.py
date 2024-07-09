c57e43911711453ae0a18c060dbe05cbda32a6ad
Indu <indulahari6@gmail.com>
2024-07-09 14:35:46 +0530

    Create PDF for each Excel file

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
+import os
 
 filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
+    # Installed openpyxl module to read data from Excel file
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+
+    #Extracting filename and invoice from paths
+    filename = Path(filepath).stem
+    #Extracting invoice from filename and splitting at date
+    invoice_no = filename.split("-")[0]
+
+    #Create PDF file from Excel file
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_no}")
+
+    #Create PDF that creates dynamic name automatically
+    pdf.output(f"PDFs/{filename}.pdf")
