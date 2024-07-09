da754a51d0ccb5fb8f0e58a30b4ae7ed5e3467fb
Indu <indulahari6@gmail.com>
2024-07-09 20:45:33 +0530

    Add invoice date

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     #Extracting invoice from filename and splitting at date
     invoice_no = filename.split("-")[0]
+    date = filename.split("-")[1]
 
     #Create PDF file from Excel file
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B", size=10)
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_no}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_no}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
     #Create PDF that creates dynamic name automatically
     pdf.output(f"PDFs/{filename}.pdf")
