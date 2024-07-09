84ca727496df690318aa41f8e3f0e9aef97defc4
Indu <indulahari6@gmail.com>
2024-07-09 23:09:04 +0530

    Add Header to table in PDF

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
-import os
 
 filepaths = glob.glob("Invoices/*.xlsx")
 
@@ -10,12 +9,12 @@ for filepath in filepaths:
     # Installed openpyxl module to read data from Excel file
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    #Extracting filename and invoice from paths
+    # Extracting filename and invoice from paths
     filename = Path(filepath).stem
     #Extracting invoice and date from filename
     invoice_no, date = filename.split("-")
 
-    #Create PDF file from Excel file
+    # Create PDF file from Excel file
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -25,14 +24,26 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=10)
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
+    # Table Header by using pandas
+    header = df.columns
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(40, 40, 40)
+    pdf.cell(w=30, h=8, txt=header[0], border=1)
+    pdf.cell(w=70, h=8, txt=header[1], border=1)
+    pdf.cell(w=30, h=8, txt=header[2], border=1)
+    pdf.cell(w=30, h=8, txt=header[3], border=1)
+    pdf.cell(w=30, h=8, txt=header[4], border=1, ln=1)
+
+    # Add rows to the table
     for index, row in df.iterrows():
         pdf.set_font(family="Times", style="B", size=10)
-        pdf.set_text_color(80, 80, 80)
+        pdf.set_text_color(40, 40, 40)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
-    #Create PDF that creates dynamic name automatically
+    # Create PDF that creates dynamic name automatically
     pdf.output(f"PDFs/{filename}.pdf")
